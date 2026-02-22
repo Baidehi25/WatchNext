3f19c3f935a49c601b7fafa96febf30e89a95fa2
Baidehi Samantray <baidehi.shreya@gmail.com>
2026-02-22 12:37:50 +0530

    edited background carousel

--- WatchNext/src/Components/BackgroundCarousel.jsx
+++ WatchNext/src/Components/BackgroundCarousel.jsx
@@ -11,23 +11,18 @@ const posters = [
 export default function BackgroundCarousel() {
   return (
     <div className="absolute inset-0 overflow-hidden z-0">
-
       <div className="flex w-max whitespace-nowrap animate-carousel will-change-transform">
-
         {[...posters, ...posters].map((src, index) => (
           <img
             key={`${src}-${index}`}
             src={src}
             alt="poster"
-            className="w-[400px] h-[100vh] object-cover opacity-80 brightness-60"
+            className="w-[200px] h-[100vh] object-cover opacity-80 brightness-60"
           />
         ))}
-
       </div>
 
       <div className="absolute inset-0 bg-black/80" />
-
     </div>
-
   );
 }
